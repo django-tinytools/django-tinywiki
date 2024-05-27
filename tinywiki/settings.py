a011d915d6aaffe320aead4b2b2a44a3cd57aa97
Christian Moser <c9moser@gmx.at>
2024-05-27 18:25:37 +0200

    2024-05-27 18:25:37 (chris-desktop)

--- tinywiki/settings.py
+++ tinywiki/settings.py
@@ -134,6 +134,8 @@ STATIC_URL = 'static/'
 MEDIA_ROOT = BASE_DIR / "media"
 MEDIA_URL = "media/"
 
+UPLOAD_ROOT = BASE_DIR / ".uploads"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
